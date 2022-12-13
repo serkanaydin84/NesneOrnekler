83dbb8546276ce7061668452c0cb33eac084410d
BIL-LAB5OGRT\Ogrenci <123@123.COM>
2022-12-13 12:17:32 +0300

    no message

--- U2_GoldSoru4/Form1.Designer.cs
+++ U2_GoldSoru4/Form1.Designer.cs
@@ -29,13 +29,125 @@ namespace U2_GoldSoru4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnKucuk = new System.Windows.Forms.TextBox();
+            this.txtEnBuyuk = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "En Küçük Sayı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "En Büyük Sayı: ";
+            // 
+            // txtEnKucuk
+            // 
+            this.txtEnKucuk.Location = new System.Drawing.Point(101, 36);
+            this.txtEnKucuk.Name = "txtEnKucuk";
+            this.txtEnKucuk.Size = new System.Drawing.Size(55, 20);
+            this.txtEnKucuk.TabIndex = 2;
+            // 
+            // txtEnBuyuk
+            // 
+            this.txtEnBuyuk.Location = new System.Drawing.Point(100, 69);
+            this.txtEnBuyuk.Name = "txtEnBuyuk";
+            this.txtEnBuyuk.Size = new System.Drawing.Size(55, 20);
+            this.txtEnBuyuk.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(187, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(85, 134);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(278, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(85, 134);
+            this.listBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Tek/Çift Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tek Sayılar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Çift Sayılar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(375, 189);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtEnBuyuk);
+            this.Controls.Add(this.txtEnKucuk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gold Soru4 / Tek-Çift";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnKucuk;
+        private System.Windows.Forms.TextBox txtEnBuyuk;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- U2_GoldSoru4/Form1.cs
+++ U2_GoldSoru4/Form1.cs
@@ -16,5 +16,42 @@ namespace U2_GoldSoru4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            int enKucuk, enBuyuk;
+
+            try
+            {
+                enKucuk = Convert.ToInt32(txtEnKucuk.Text);
+                enBuyuk = Convert.ToInt32(txtEnBuyuk.Text);
+
+                if (enKucuk >= enBuyuk)
+                {
+                    MessageBox.Show("Lütfen Küçük sayıyı, küçük sayı olarak giriniz.");
+                }
+                else
+                {
+                    for (int i = enKucuk; i <= enBuyuk; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            listBox1.Items.Add(i);
+                        }
+                        else
+                        {
+                            listBox2.Items.Add(i);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen sayı giriniz.");
+            }
+        }
     }
 }
