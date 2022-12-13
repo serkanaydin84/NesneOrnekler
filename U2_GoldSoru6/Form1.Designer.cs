3e1a84f57b36a1edb1b287dbc3450709add1d8a1
BIL-LAB5OGRT\Ogrenci <123@123.COM>
2022-12-13 15:46:01 +0300

    no message

---
+++ U2_GoldSoru6/Form1.Designer.cs
@@ -0,0 +1,410 @@
+﻿
+namespace U2_GoldSoru6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNot3 = new System.Windows.Forms.TextBox();
+            this.txtNot1 = new System.Windows.Forms.TextBox();
+            this.txtNot2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbRobotik = new System.Windows.Forms.RadioButton();
+            this.rbNesne = new System.Windows.Forms.RadioButton();
+            this.rbTurkce = new System.Windows.Forms.RadioButton();
+            this.rbMat = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label9 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnHesapla);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtNot3);
+            this.groupBox2.Controls.Add(this.txtNot1);
+            this.groupBox2.Controls.Add(this.txtNot2);
+            this.groupBox2.Location = new System.Drawing.Point(176, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 162);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hesaplama";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yazılı 1: ";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnHesapla.Location = new System.Drawing.Point(9, 124);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(220, 32);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Ortalama Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(185, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yazılı 2: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Labe4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Yazılı 3: ";
+            // 
+            // txtNot3
+            // 
+            this.txtNot3.Location = new System.Drawing.Point(76, 65);
+            this.txtNot3.Name = "txtNot3";
+            this.txtNot3.Size = new System.Drawing.Size(153, 20);
+            this.txtNot3.TabIndex = 6;
+            // 
+            // txtNot1
+            // 
+            this.txtNot1.Location = new System.Drawing.Point(76, 13);
+            this.txtNot1.Name = "txtNot1";
+            this.txtNot1.Size = new System.Drawing.Size(153, 20);
+            this.txtNot1.TabIndex = 4;
+            // 
+            // txtNot2
+            // 
+            this.txtNot2.Location = new System.Drawing.Point(76, 39);
+            this.txtNot2.Name = "txtNot2";
+            this.txtNot2.Size = new System.Drawing.Size(153, 20);
+            this.txtNot2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbRobotik);
+            this.groupBox1.Controls.Add(this.rbNesne);
+            this.groupBox1.Controls.Add(this.rbTurkce);
+            this.groupBox1.Controls.Add(this.rbMat);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(158, 162);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dersler";
+            // 
+            // rbRobotik
+            // 
+            this.rbRobotik.AutoSize = true;
+            this.rbRobotik.Location = new System.Drawing.Point(6, 91);
+            this.rbRobotik.Name = "rbRobotik";
+            this.rbRobotik.Size = new System.Drawing.Size(106, 17);
+            this.rbRobotik.TabIndex = 3;
+            this.rbRobotik.TabStop = true;
+            this.rbRobotik.Text = "Robotik Kodlama";
+            this.rbRobotik.UseVisualStyleBackColor = true;
+            // 
+            // rbNesne
+            // 
+            this.rbNesne.AutoSize = true;
+            this.rbNesne.Location = new System.Drawing.Point(6, 68);
+            this.rbNesne.Name = "rbNesne";
+            this.rbNesne.Size = new System.Drawing.Size(122, 17);
+            this.rbNesne.TabIndex = 2;
+            this.rbNesne.TabStop = true;
+            this.rbNesne.Text = "Nesne Tabanlı Prog.";
+            this.rbNesne.UseVisualStyleBackColor = true;
+            // 
+            // rbTurkce
+            // 
+            this.rbTurkce.AutoSize = true;
+            this.rbTurkce.Location = new System.Drawing.Point(6, 42);
+            this.rbTurkce.Name = "rbTurkce";
+            this.rbTurkce.Size = new System.Drawing.Size(59, 17);
+            this.rbTurkce.TabIndex = 1;
+            this.rbTurkce.TabStop = true;
+            this.rbTurkce.Text = "Türkçe";
+            this.rbTurkce.UseVisualStyleBackColor = true;
+            // 
+            // rbMat
+            // 
+            this.rbMat.AutoSize = true;
+            this.rbMat.Location = new System.Drawing.Point(6, 19);
+            this.rbMat.Name = "rbMat";
+            this.rbMat.Size = new System.Drawing.Size(74, 17);
+            this.rbMat.TabIndex = 0;
+            this.rbMat.TabStop = true;
+            this.rbMat.Text = "Matematik";
+            this.rbMat.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Matematik";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(101, 180);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(101, 209);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(280, 23);
+            this.progressBar2.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Türkçe";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(101, 238);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(280, 23);
+            this.progressBar3.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Nesne";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(101, 267);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(280, 23);
+            this.progressBar4.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Robotik";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(101, 296);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(280, 23);
+            this.progressBar5.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 303);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Sene Sonu Ort: ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(386, 187);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "      ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Green;
+            this.label12.Location = new System.Drawing.Point(386, 216);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(25, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "      ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Blue;
+            this.label13.Location = new System.Drawing.Point(386, 245);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "      ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label14.Location = new System.Drawing.Point(386, 277);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(25, 13);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "      ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 340);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "GoldSoru6";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNot3;
+        private System.Windows.Forms.TextBox txtNot1;
+        private System.Windows.Forms.TextBox txtNot2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbRobotik;
+        private System.Windows.Forms.RadioButton rbNesne;
+        private System.Windows.Forms.RadioButton rbTurkce;
+        private System.Windows.Forms.RadioButton rbMat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ U2_GoldSoru6/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_GoldSoru6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rbMat_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNot1.Clear();
+            txtNot2.Clear();
+            txtNot3.Clear();
+            label4.Text = "Matematik ortalaması : ";
+        }
+
+        private void rbTurkce_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNot1.Text = "";
+            txtNot2.Text = "";
+            txtNot3.Text = "";
+            label4.Text = "Türkçe ortalaması : ";
+        }
+
+        private void rbNesne_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNot1.Text = "";
+            txtNot2.Text = "";
+            txtNot3.Text = "";
+            label4.Text = "Nesne ortalaması : ";
+        }
+
+        private void rbRobotik_CheckedChanged(object sender, EventArgs e)
+        {
+            txtNot1.Text = "";
+            txtNot2.Text = "";
+            txtNot3.Text = "";
+            label4.Text = "Robotik ortalaması : ";
+        }
+
+        int seneSonu = 0;
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int not1, not2, not3, ort;
+            progressBar1.ForeColor = Color.Red;
+            progressBar2.ForeColor = Color.Green;
+            progressBar3.ForeColor = Color.Blue;
+            progressBar4.ForeColor = Color.Black;
+
+            try
+            {
+                not1 = Convert.ToInt32(txtNot1.Text);
+                not2 = Convert.ToInt32(txtNot2.Text);
+                not3 = Convert.ToInt32(txtNot3.Text);
+
+                if (not1 > 100 || not1 < 0 || not2 > 100 || not2 < 0 || not3 > 100 || not3 < 0)
+                {
+                    MessageBox.Show("Lütfen 0-100 arasında rakam giriniz.");
+                }
+                else
+                {
+                    ort = (not1 + not2 + not3) / 3;
+                    label5.Text = ort.ToString();
+
+                    if (rbMat.Checked)
+                    {
+                        progressBar1.Value = ort;
+                        seneSonu += ort;
+                    }
+                    else if (rbTurkce.Checked)
+                    {
+                        progressBar2.Value = ort;
+                        seneSonu += ort;
+                    }
+                    else if (rbNesne.Checked)
+                    {
+                        progressBar3.Value = ort;
+                        seneSonu += ort;
+                    }
+                    else if (rbRobotik.Checked)
+                    {
+                        progressBar4.Value = ort;
+                        seneSonu += ort;
+                    }
+
+                    progressBar5.ForeColor = Color.Brown;
+                    progressBar5.Value = seneSonu / 4;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen sayı giriniz.");
+            }
+        }
+    }
+}
