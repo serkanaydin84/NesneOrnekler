3058f9fb3e6cd39c64c5be096530383ed9f4bfe2
BIL-LAB5OGRT\Ogrenci <123@123.COM>
2022-12-27 16:13:46 +0300

    Ders Sonu Sınıflara giriş yapıldı

---
+++ U3_S77/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(8, 10);
+
+            Console.WriteLine("Dikdörtgenin Alanı: " + dikdortgen.AlanHesapla());
+            Console.WriteLine("Dikdörtgenin Çevresi: " + dikdortgen.CevreHesapla());
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class Dikdortgen
+    {
+        private int a, b;
+
+        public Dikdortgen(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
---
+++ U3_SS78/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire daire = new Daire(15);
+
+            Console.WriteLine("Dairenin Alanı: " + daire.AlanHesapla());
+            Console.WriteLine("Dairenin Çevresi: " + daire.CevreHesapla());
+            Console.ReadLine();
+        }
+    }
+
+
+    class Daire
+    {
+        private int r;
+        private double pi = 3.14;
+
+        public Daire(int r)
+        {
+            this.r = r;
+        }
+
+        public double AlanHesapla()
+        {
+            return pi * r * r;
+        }
+
+        public double CevreHesapla()
+        {
+            return 2 * pi * r;
+        }
+    }
+
+}
